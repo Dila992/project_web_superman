07de9daad4e3e9fb6d4bbbab7c5f96eba69cb681
Rifqifauu <rifqifauzi369@gmail.com>
2023-11-27 09:49:22 +0700

    insert untuk info film

--- add-admin.php
+++ add-admin.php
@@ -30,6 +30,7 @@ mysqli_close($koneksi);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Admin</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!-- Form untuk add admin di sini -->

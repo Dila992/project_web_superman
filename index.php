e86cbd56714018313d5d968f38a7592539c1dfbc
Rifqifauu <rifqifauzi369@gmail.com>
2023-12-01 10:14:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Superman</title>
 </head>
 <body>
-    
+    <!-- Halaman Utama atau Home di Sini -->
 </body>
 </html>
